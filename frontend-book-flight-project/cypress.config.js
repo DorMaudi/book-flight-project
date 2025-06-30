8652b826df13ee031c6cbc189cf2992221c56579
Dor Maudi <dor.maudi9@gmail.com>
2025-06-30 15:28:22 +0300

    Update cypress.config.js

--- frontend-book-flight-project/cypress.config.js
+++ frontend-book-flight-project/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'https://book-flight-project-1.onrender.com',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false
   }

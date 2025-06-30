37ac57b3b8e5ebe4055b838cf25aa15a6660e836
danielbogus99 <danik444@walla.co.il>
2025-06-30 19:15:50 +0300

    .

--- frontend-book-flight-project/cypress.config.js
+++ frontend-book-flight-project/cypress.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'https://book-flight-project.onrender.com'
+,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false
   }
